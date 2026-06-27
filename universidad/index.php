cf002c0325edf14bc96a10f278fe5e6fc9e64d6c
Andersonchv <andersonchinchillav@gmail.com>
2026-06-26 22:01:50 -0600

    arreglo de inicio de sesion y estilos

--- universidad/index.php
+++ universidad/index.php
@@ -1,21 +1,18 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <title>Universidad</title>
     <link rel="stylesheet" href="styles.css">
 </head>
-
 <body>
     <div class="card">
-        <h1 class="sesion">INICIO DE SESION</h1>
+        <h1 class="sesion">INICIO DE SESIÓN</h1>
  <h2 class="sesiontext">Usuario</h2>
  <input type="text" name="usuario" class="usuario">
  <h2 class="sesiontext">Contraseña</h2>
- <input type="password" name="contraseña" class="contrasena">
+ <input type="password" name="contrasena" class="contraseña">
  <button class="boton">Iniciar Sesion</button>
     </div>
 </body>
-
 </html>
