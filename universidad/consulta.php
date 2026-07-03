310a82e62ab4062c7400eb2d3674aaee3edd22d4
Anderson <122390467+Andersonchv@users.noreply.github.com>
2026-07-03 17:56:55 -0600

    Arreglo en consultas y index

--- universidad/consulta.php
+++ universidad/consulta.php
@@ -1,28 +1,119 @@
 <?php
+
+/* Conexion de sesion */
 require_once 'config.php';
 session_start();
+
+/* Datos del Formulario */
 $usuario_input = isset($_POST['usuario']) ? trim($_POST['usuario']) : '';
 $contrasena_input = isset($_POST['contrasena']) ? $_POST['contrasena'] : '';
 
+/* Validacion que los Campos No esten Vacios*/
 if ($usuario_input === '' || $contrasena_input === '') {
-    $error = 'Usuario o contraseña faltantes';
-} else {
-    $stmt = $pdo->prepare("SELECT * FROM inicio_sesion WHERE usuario = :usuario");
-    $stmt->execute(['usuario' => $usuario_input]);
-    $row = $stmt->fetch();
 
-    if ($row && isset($row['contrasena']) && password_verify($contrasena_input, $row['contrasena'])) {
-        session_regenerate_id(true);
-        $_SESSION['user_id'] = $row['cedula'];
-        $_SESSION['username'] = $row['usuario'];
-
-        header('Location: registro.php');
-        exit;
-    } 
-    else {
-        $error = 'Usuario o contraseña incorrectos';
-        exit($error);
-    }
+    $_SESSION['error'] = 'Usuario o contraseña faltantes';
+    header('Location: index.php');
+    exit;
 }
 
-?>
+/* Busqueda del Usuario en BD */
+$stmt = $pdo->prepare("
+    SELECT *
+    FROM inicio_sesion
+    WHERE usuario = :usuario
+");
+
+$stmt->execute([
+    'usuario' => $usuario_input
+]);
+
+$row = $stmt->fetch();
+
+
+/* Si usuario no Existe*/
+if (!$row) {
+
+    $_SESSION['error'] = 'Usuario o contraseña incorrectos';
+    header('Location: index.php');
+    exit;
+}
+
+/* Verificar si el usuario ya está bloqueado */
+if (
+    isset($row['estado']) &&
+    $row['estado'] === 'B'
+) {
+
+    $_SESSION['error'] = 'Usuario bloqueado por demasiados intento fallidos';
+    header('Location: index.php');
+    exit;
+}
+
+/* Verificar contraseña */
+if (
+    isset($row['contrasena']) &&
+    password_verify($contrasena_input, $row['contrasena'])
+) {
+
+     /*Reiniciar contador de intentos cuando digita bien la contraseña*/
+    $stmt = $pdo->prepare("
+        UPDATE inicio_sesion
+        SET intentos = 0
+        WHERE usuario = :usuario
+    ");
+
+    $stmt->execute([
+        'usuario' => $usuario_input
+    ]);
+
+    session_regenerate_id(true);
+
+    $_SESSION['user_id'] = $row['cedula'];
+    $_SESSION['username'] = $row['usuario'];
+    
+    
+    /* Redirige la pagina */
+    header('Location: inicio.php');
+    exit;
+}
+
+/* Incrementa cantidad de intentos*/
+$intento = $row['intentos'] + 1;
+
+if ($intento > 3) {
+    $intento = 3;
+}
+
+/* Actualiza el contador*/
+$stmt = $pdo->prepare("
+    UPDATE inicio_sesion
+    SET intentos = :intento
+    WHERE usuario = :usuario
+");
+
+$stmt->execute([
+    'intento' => $intento,
+    'usuario' => $usuario_input
+]);
+
+/* Bloquedo de Usuario */
+if ($intento > 3) {
+
+    $stmt = $pdo->prepare("
+        UPDATE inicio_sesion
+        SET estado = 'B'
+        WHERE usuario = :usuario
+    ");
+
+    $stmt->execute([
+        'usuario' => $usuario_input
+    ]);
+
+    $_SESSION['error'] = 'Usuario bloqueado por demasiados intento fallidos';
+    header('Location: index.php');
+    exit;
+}
+
+$_SESSION['error'] = "Usuario o contraseña incorrectos. Queda $intento intentos de 3";
+header('Location: index.php');
+exit;
--- universidad/index.php
+++ universidad/index.php
@@ -1,21 +1,44 @@
+<?php
+session_start();
+
+$error = "";
+
+if (isset($_SESSION['error'])) {
+    $error = $_SESSION['error'];
+    unset($_SESSION['error']);
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <title>Universidad</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
-    <div class="card">
+       <div class="card">
         <form action="consulta.php" method="post">
         <h1 class="sesion">INICIO DE SESIÓN</h1>
+      
         <h2 class="sesiontext">Usuario</h2>
         <input type="text" name="usuario" class="usuario" required>
         <h2 class="sesiontext">Contraseña</h2>
         <input type="password" name="contrasena" class="contraseña" required>
         <button class="boton_iniciar" type="submit">Iniciar Sesion</button>
+       
+    
+        <?php
+        // muestra el error
+        if ($error != "") { ?>
+                <div>
+                    <?php echo htmlspecialchars($error); ?>
+                </div>
+            <?php } ?>
         <a href="registro.php" class="enla_registrarse">Registrarse</a>
     </form>
     </div>
 </body>
+
 </html>
